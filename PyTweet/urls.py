ec9f8d41efb9f19edf009f3fe658062f97ddfc6a
Clément Fasquel <contact@clement-fasquel.fr>
2014-04-19 18:48:37 +0200

    first view

--- PyTweet/urls.py
+++ PyTweet/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PyTweet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'PyTweetApp.views.home'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- PyTweetApp/views.py
+++ PyTweetApp/views.py
@@ -1,3 +1,9 @@
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+	text = """<h1>Bienvenue sur PyTweet !</h1><p>Voici la première vue de l'application.</p>"""
+  	return HttpResponse(text)
