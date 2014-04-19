54e7ecb14da387d50ea8fc82345d1146d57946e1
Clément Fasquel <contact@clement-fasquel.fr>
2014-04-19 20:37:29 +0200

    first template

--- PyTweetApp/views.py
+++ PyTweetApp/views.py
@@ -5,5 +5,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	text = """<h1>Bienvenue sur PyTweet !</h1><p>Voici la première vue de l'application.</p>"""
-  	return HttpResponse(text)
+  	return render(request, 'home.html', locals())
