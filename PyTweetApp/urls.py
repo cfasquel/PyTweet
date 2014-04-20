caaf18fa254cdb20e58ad1e40694a0137862f70f
Clément Fasquel <contact@clement-fasquel.fr>
2014-04-20 15:15:08 +0200

    template hashtag

--- PyTweetApp/urls.py
+++ PyTweetApp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('PyTweetApp.views',
+    url(r'^Hashtag/$', 'hashtag'),
     url(r'^$', 'home'),
 )
--- PyTweetApp/views.py
+++ PyTweetApp/views.py
@@ -6,9 +6,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
 
-	isConnected = True
+	isConnected = False
 
 	if isConnected :
 		return render(request, 'tweet-line.html', locals())
   	else :
   		return render(request, 'home.html', locals())
+
+def hashtag(request):
+
+	return render(request, 'hashtag.html', locals())
