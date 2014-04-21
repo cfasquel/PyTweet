bfcc9ad0426331105f24c721c33f107ee6d1af74
Clément Fasquel <contact@clement-fasquel.fr>
2014-04-21 15:03:13 +0200

    connexion page

--- PyTweetApp/views.py
+++ PyTweetApp/views.py
@@ -2,16 +2,28 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from PyTweetApp.forms import ConnexionForm
 
 # Create your views here.
+
 def home(request):
 
-	isConnected = False
+    if request.method == "POST":
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]  # Nous récupérons le nom d'utilisateur
+            password = form.cleaned_data["password"]  # … et le mot de passe
+            user = authenticate(username=username, password=password)  #Nous vérifions si les données sont correctes
+            if user:  # Si l'objet renvoyé n'est pas None
+                login(request, user)  # nous connectons l'utilisateur
+                return render(request, 'tweet-line.html', locals())
+            else: #sinon une erreur sera affichée
+                error = True
+    else:
+        form = ConnexionForm()
 
-	if isConnected :
-		return render(request, 'tweet-line.html', locals())
-  	else :
-  		return render(request, 'home.html', locals())
+    return render(request, 'home.html',locals())
 
 def hashtag(request):
 
