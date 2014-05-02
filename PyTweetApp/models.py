bc54757c952247093981da667d5bfb357053e19d
Clément Fasquel <contact@clement-fasquel.fr>
2014-05-02 15:22:43 +0200

    corrected models.py

--- PyTweetApp/models.py
+++ PyTweetApp/models.py
@@ -2,6 +2,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Mention(models.Model):
+	mentionned_user = models.ForeignKey(User)
+
+class Hashtag(models.Model):
+	name = models.TextField(max_length=40)
+
 class Tweet(models.Model):
 	author = models.ForeignKey(User)
 	mentions = models.ManyToManyField(Mention, through='TweetMention')
@@ -9,19 +15,10 @@ class Tweet(models.Model):
 	text = models.TextField(max_length=140)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False)
 
-class Mention(models.Model):
-	mentionned_user = models.ForeignKey(User)
-
 class TweetMention(models.Model):
 	tweet = models.ForeignKey(Tweet)
 	mention = models.ForeignKey(Mention)
 
-class Hashtag(models.Model):
-	name = models.TextField(max_length=40)
-
 class TweetHashtag(models.Model):
 	tweet = models.ForeignKey(Tweet)
 	hashtag = models.ForeignKey(Hashtag)
-
-def __unicode__(self):
-		return self.text
